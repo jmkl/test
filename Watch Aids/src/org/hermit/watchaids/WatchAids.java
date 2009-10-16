b4a48c3faad91c73fac1957da0153c6cc077309b
headstay <headstay@3248975e-79f9-11de-8a13-ad0aff062799>
2009-10-16 00:25:41 +0000

    Initial import.

---
+++ Watch Aids/src/org/hermit/watchaids/WatchAids.java
@@ -0,0 +1,14 @@
+package org.hermit.watchaids;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WatchAids extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
+
