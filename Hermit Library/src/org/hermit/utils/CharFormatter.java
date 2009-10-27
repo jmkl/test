c668cc2ee3d46d198220489ff4b157991722b93f
headstay <headstay@3248975e-79f9-11de-8a13-ad0aff062799>
2009-10-27 02:10:03 +0000

    Formatters.

--- Hermit Library/src/org/hermit/utils/CharFormatter.java
+++ Hermit Library/src/org/hermit/utils/CharFormatter.java
@@ -21,18 +21,10 @@
 package org.hermit.utils;
 
 
-import static java.lang.Math.PI;
-import static java.lang.Math.abs;
-import static java.lang.Math.round;
-import static java.lang.Math.toDegrees;
-import static java.lang.Math.toRadians;
-
-import java.text.NumberFormat;
-
-
 /**
- * Utilities for handling and formatting angles, including latitudes
- * and longitudes.
+ * Utilities for quickly formatting numbers into character buffers, without
+ * memory allocations.  These routines are much faster than using
+ * String.format, and can be used to avoid GC.
  *
  * @author	Ian Cameron Smith
  */
@@ -40,29 +32,11 @@ public class CharFormatter
 {
 
 	// ******************************************************************** //
-	// Public Constants.
+	// Private Constructors.
 	// ******************************************************************** //
 
 	/**
-	 * Half pi; a quarter circle in radians; same as 90 degrees.
-	 */
-	public static final double HALFPI = PI / 2;
-
-
-	/**
-	 * Two times pi; a circle in radians; same as 360 degrees.
-	 */
-	public static final double TWOPI = PI * 2;
-
-
-	// ******************************************************************** //
-	// Public Constructors.
-	// ******************************************************************** //
-
-	/**
-	 * Create an Angle from an angle given in radians.
-	 * 
-	 * @param	radians		Source angle in radians.
+	 * No instances of this class.
 	 */
 	private CharFormatter() {
 	}
@@ -73,223 +47,175 @@ public class CharFormatter
 	// ************************************************************************ //
 
     /**
-     * Format a float to a field width of 6, with 1
-     * decimals.  MUCH faster than String.format.
+     * Format an integer into a fixed-width field.  MUCH faster
+     * than String.format.
+     * 
+     * @param   buf         Buffer to place the result in.
+     * @param   off         Offset within buf to start writing at.
+     * @param   val         The value to format.
+     * @param   field       Width of the field to format in.
+     * @param   signed      Iff true, add a sign character, space for
+     *                      positive, '-' for negative.  This takes
+     *                      up one place in the given field width.
      */
     public static final void formatInt(char[] buf, int off, int val,
                                        int field, boolean signed)
     {
-        int intDigits = field - (signed ? 1 : 0);
         int sign = val >= 0 ? 1 : -1;
         val *= sign;
-        
-        int last = 0;
-        for (int i = off + field - 1; i >= off + field - intDigits; --i) {
-            if (val == 0 && i < off + field - 1) {
-                buf[i] = ' ';
-            } else {
-                buf[i] = (char) ('0' + val % 10);
-                val /= 10;
-                last = i;
-            }
-        }
-        if (signed)
-            buf[last - 1] = sign > 0 ? ' ' : '-';
+        char schar = signed ? (sign < 0 ? '-' : ' ') : 0;
+        formatInt(buf, off, val, field, schar, false);
     }
 
 
     /**
-     * Format a float to a field width of 6, with 1
-     * decimals.  MUCH faster than String.format.
+     * Format a floating-point value into a fixed-width field.  MUCH faster
+     * than String.format.
+     * 
+     * @param   buf         Buffer to place the result in.
+     * @param   off         Offset within buf to start writing at.
+     * @param   val         The value to format.
+     * @param   field       Width of the field to format in.
+     * @param   frac        Number of digits after the decimal.
+     * @param   signed      Iff true, add a sign character, space for
+     *                      positive, '-' for negative.  This takes
+     *                      up one place in the given field width.
      */
-    public static final void formatFloat(char[] buf, int off, double val, int field, int frac, boolean signed) {
-        int intDigits = field - frac - (signed ? 2 : 1);
-        
+    public static final void formatFloat(char[] buf, int off, double val,
+                                         int field, int frac, boolean signed)
+    {
+        int intDigits = field - frac - 1;
+        int sign = val >= 0 ? 1 : -1;
+        val *= sign;
+        char schar = signed ? (sign < 0 ? '-' : ' ') : 0;
         int intPart = (int) val;
-        int fracPart = (int) ((val - intPart) * 10);
+        double fracPart = val - intPart;
+        for (int i = 0; i < frac; ++i)
+            fracPart *= 10;
 
-        formatInt(buf, off, intPart, field - frac - 1, signed);
-                
-        String b = "" + intPart;
-        String a = "" + fracPart;
-        StringBuilder res = new StringBuilder("    .0");
-        int bs = intDigits - b.length();
-        res.replace((bs < 0 ? 0 : bs), intDigits, b);
-        res.replace(field - a.length(), field, a);
-        return res.toString();
+        formatInt(buf, off, intPart, intDigits, schar, false);
+        buf[off + intDigits] = '.';
+        formatInt(buf, off + intDigits + 1, (int) fracPart, frac, (char) 0, true);
     }
 
-   
-	/**
-	 * Format an angle as a bearing.
-	 *
-	 * @param	val			The value to format.
-	 * @return				The formatted value.
-	 */
-	public static String formatBearing(double val) {
-		return Math.round(val) + "°";
-	}
+
+    /**
+     * Internal integer formatter.
+     * 
+     * @param   buf         Buffer to place the result in.
+     * @param   off         Offset within buf to start writing at.
+     * @param   val         The value to format.  Must not be negative.
+     * @param   field       Width of the field to format in.
+     * @param   schar       Iff not zero, add this sign character.  This takes
+     *                      up one place in the given field width.
+     * @param   leadZero    Iff true, pad on the left with leading zeros
+     *                      instead of spaces.
+     */
+    private static final void formatInt(char[] buf, int off, int val,
+                                        int field, char schar, boolean leadZero)
+    {
+        int intDigits = field - (schar != 0 ? 1 : 0);
+        
+        int last = 0;
+        for (int i = off + field - 1; i >= off + field - intDigits; --i) {
+            if (val == 0 && !leadZero && i < off + field - 1) {
+                buf[i] = ' ';
+            } else {
+                buf[i] = val == 0 ? '0' : (char) ('0' + val % 10);
+                val /= 10;
+                last = i;
+            }
+        }
+        if (schar != 0) {
+            buf[off] = ' ';
+            buf[last - 1] = schar;
+        }
+    }
 
 
 	/**
 	 * Format an angle for user display in degrees and minutes.
-	 * Negative angles are formatted with a "-" sign.
-	 *
+	 * Negative angles are formatted with a "-" sign, as in
+	 * "-171°15.165'".  Place the result in the supplied buffer.
+     *
+     * @param   buf         Buffer to place the result in.
+     * @param   off         Offset within buf to start writing at.
 	 * @param	angle		The angle to format.
-	 * @return				The formatted angle.
+     * @return              Number of characters written.
 	 */
-	public static String formatDegMin(double angle) {
-		return formatDegMin(angle, ' ', '-');
+	public static int formatDegMin(char[] buf, int off, double angle) {
+		return formatDegMin(buf, off, angle, ' ', '-', false);
 	}
 
 
 	/**
-	 * Format a latitude or longitude angle as a string in the format
-	 * "W171° 15.165'".
-	 *
-	 * @param	angle		Angle to format.
+	 * Format an angle as a string in the format
+	 * "W171°15.165'".  Place the result in the supplied buffer.
+	 * 
+     * @param   buf         Buffer to place the result in.
+     * @param   off         Offset within buf to start writing at.
+     * @param   angle       The angle to format.
 	 * @param	pos			Sign character to use if positive.
 	 * @param	neg			Sign character to use if negative.
-	 * @return				The formatted angle.
+     * @param   space       If true, leave a space after the sign and degrees.
+     *                      Otherwise pack them.
+     * @return              Number of characters written.
 	 */
-	public static String formatDegMin(double angle, char pos, char neg) {
-		StringBuilder sb = new StringBuilder(12);
-		formatDegMin(angle, pos, neg, sb);
-		return sb.toString();
-	}
-
-
-	/**
-	 * Format a latitude or longitude angle as a string in the format
-	 * "W171°15.165'".  Place the result in a supplied StringBuilder.
-	 * 
-	 * The StringBuilder will be set to the required length, 12.  For 
-	 * best efficiency, leave it at that length.
-	 * 
-	 * @param	angle		Angle to format.
-	 * @param	pos			Sign character to use if positive.
-	 * @param	neg			Sign character to use if negative.
-	 * @param	sb			StringBuilder to write the result into.
-	 */
-	public static void formatDegMin(double angle,
-									char pos, char neg, StringBuilder sb)
+	public static int formatDegMin(char[] buf, int off, double angle,
+									char pos, char neg, boolean space)
 	{
-		if (sb.length() != 12)
-			sb.setLength(12);
-		
+	    int p = off;
+	    
 		if (angle < 0) {
-			sb.setCharAt(0, neg);
+			buf[p++] = neg;
 			angle = -angle;
 		} else
-			sb.setCharAt(0, pos);
-	
+            buf[p++] = pos;
+		if (space)
+            buf[p++] = ' ';
+		
 		int deg = (int) angle;
 		int min = (int) (angle * 60.0 % 60.0);
 		int frac = (int) (angle * 60000.0 % 1000.0);
-
-		sb.setCharAt( 1, deg < 100 ? ' ' : (char) ('0' + deg / 100));
-		sb.setCharAt( 2, deg < 10 ? ' ' : (char) ('0' + deg / 10 % 10));
-		sb.setCharAt( 3, (char) ('0' + deg % 10));
-		sb.setCharAt( 4, '°');
-		sb.setCharAt( 5, (char) ('0' + min / 10));
-		sb.setCharAt( 6, (char) ('0' + min % 10));
-		sb.setCharAt( 7, '.');
-		sb.setCharAt( 8, (char) ('0' + frac / 100));
-		sb.setCharAt( 9, (char) ('0' + frac / 10 % 10));
-		sb.setCharAt(10, (char) ('0' + frac % 10));
-		sb.setCharAt(11, '\'');
-	}
-	
-
-	/**
-	 * Format an angle for user display in degrees and minutes.
-	 *
-	 * @param	angle		The angle to format.
-	 * @return				The formatted angle.
-	 */
-	public static String formatDegMinSec(double angle) {
-		return formatDegMinSec(angle, ' ', '-');
-	}
-	
-	
-	/**
-	 * Format an angle for user display in degrees and minutes.
-	 *
-	 * @param	angle		The angle to format.
-	 * @param	posSign		Sign to use for positive values; none if null.
-	 * @param	negSign		Sign to use for negative values; none if null.
-	 * @return				The formatted angle.
-	 */
-	public static String formatDegMinSec(double angle, char posSign, char negSign) {
-		char sign = angle >= 0 ? posSign : negSign;
-		angle = Math.abs(angle);
-
-		int deg = (int) angle;
-		angle = (angle - deg) * 60.0;
-		int min = (int) angle;
-		angle = (angle - min) * 60.0;
-		double sec = angle;
-
-		// Rounding errors?
-		if (sec >= 60.0) {
-			sec = 0;
-			++min;
-		}
-		if (min >= 60) {
-			min -= 60;
-			++deg;
-		}
 		
-		return String.format("%s%3d° %2d' %8.5f\"", sign, deg, min, sec);
-		//return sign + deg + "° " + min + "' " + angleFormat.format(sec) + "\"";
+        formatInt(buf, p, deg, 3, (char) 0, false);
+        p += 3;
+        buf[p++] = '°';
+        if (space)
+            buf[p++] = ' ';
+        
+        formatInt(buf, p, min, 2, (char) 0, true);
+        p += 2;
+        buf[p++] = '.';
+        
+        formatInt(buf, p, frac, 3, (char) 0, true);
+        p += 2;
+        buf[p++] = '\'';
+        
+        return p - off;
 	}
-
+	
 
 	/**
 	 * Format a latitude and longitude for user display in degrees and
-	 * minutes.
+	 * minutes.  Place the result in the supplied buffer.
 	 *
+     * @param   buf         Buffer to place the result in.
+     * @param   off         Offset within buf to start writing at.
 	 * @param	lat			The latitude.
 	 * @param	lon			The longitude.
-	 * @return				The formatted angle.
+     * @param   space       If true, leave a space after the sign and degrees.
+     *                      Otherwise pack them.
+	 * @return				Number of characters written.
 	 */
-	public static String formatLatLon(double lat, double lon) {
-		return formatDegMin(lat, 'N', 'S') + " " + formatDegMin(lon, 'E', 'W');
-	}
-
-	
-	/**
-	 * Format an angle for user display as a right ascension.
-	 *
-	 * @param	angle		The angle to format.
-	 * @return				The formatted angle.
-	 * TODO: units!
-	 */
-	public static String formatRightAsc(double angle) {
-		if (angle < 0)
-			angle += 360.0;
-		double hours = angle / 15.0;
-		
-		int h = (int) hours;
-		hours = (hours - h) * 60.0;
-		int m = (int) hours;
-		hours = (hours - m) * 60.0;
-		double s = hours;
-
-		// Rounding errors?
-		if (s >= 60.0) {
-			s = 0;
-			++m;
-		}
-		if (m >= 60) {
-			m -= 60;
-			++h;
-		}
-		if (h >= 24) {
-			h -= 24;
-		}
-		
-		return String.format("%02dh %02d' %08.5f\"", h, m, s);
+	public static int formatLatLon(char[] buf, int off, double lat, double lon, boolean space) {
+        int p = off;
+        
+		p += formatDegMin(buf, off, lat, 'N', 'S', space);
+		buf[p++] = ' ';
+		p += formatDegMin(buf, off, lon, 'E', 'W', space);
+        
+        return p - off;
 	}
 
 }
--- Hermit Library/test/org/hermit/test/utils/FormatterTests.java
+++ Hermit Library/test/org/hermit/test/utils/FormatterTests.java
@@ -32,64 +32,139 @@ import org.hermit.utils.CharFormatter;
  * @author	Ian Cameron Smith
  */
 public class FormatterTests
-	extends TestCase
+    extends TestCase
 {
 
-	// ******************************************************************** //
-	// Test Definitions.
-	// ******************************************************************** //
+    // ******************************************************************** //
+    // Test Framework.
+    // ******************************************************************** //
 
-	private static final class TestData {
-		public TestData(int val, int field, boolean signed, String res) {
-			this.val = val;
-			this.field = field;
-			this.signed = signed;
-			this.expect = res;
-		}
-
-		private void run(char[] buf, int off) {
-		    CharFormatter.formatInt(buf, off, val, field, signed);
-		    String res = new String(buf, off, field);
-		    assertEquals(expect, res);
-		}
-
-		private int val;
-		private int field;
-		private boolean signed;
-		private String expect;
-	}
-	
-	
-	/**
-	 * Table of test data.  Each row contains:
-	 *   - Test name, ellipsoid to use
-	 *   - Station 1 latitude, degrees; longitude, degrees
-	 *   - Station 2 latitude, degrees; longitude, degrees
-	 *   - Forward azimuth, degrees; back azimuth, degrees
-	 *   - Distance, metres
-	 */
-	private static final TestData[] testData = {
-		new TestData(173, 7, false, "    173"),
-        new TestData(173, 3, false, "173"),
-        new TestData(173, 3, true, " 73"),
-        new TestData(173, 2, true, " 3"),
-        new TestData(-173, 7, false, "    173"),
-        new TestData(-173, 7, true, "   -173"),
-        new TestData(-173, 3, false, "173"),
-        new TestData(-173, 3, true, "-73"),
-        new TestData(-173, 2, true, "-3"),
-	};
+    protected void setUp() {
+        buf = new char[63];
+    }
 
 
-	// ******************************************************************** //
-	// Tests.
-	// ******************************************************************** //
+    private void run(int off, int val, int field, boolean signed, String expect) {
+        CharFormatter.formatInt(buf, off, val, field, signed);
+        String res = new String(buf, off, field);
+        assertEquals(expect, res);
+    }
 
-	public void testFormatInt() {
-	    char[] buf = new char[63];
-		for (TestData test : testData)
-			test.run(buf, 13);
-	}
-		
+    private void run(int off, double val, int field, int frac, boolean signed, String expect) {
+        CharFormatter.formatFloat(buf, off, val, field, frac, signed);
+        String res = new String(buf, off, field);
+        assertEquals(expect, res);
+    }
+
+    
+    // ******************************************************************** //
+    // Integer Tests.
+    // ******************************************************************** //
+
+    public void testPosIntUns() {
+        run(13, 173, 7, false, "    173");
+        run(13, 173, 3, false, "173");
+    }
+
+
+    public void testZeroIntUns() {
+        run(13, 0, 7, false, "      0");
+        run(13, 0, 1, false, "0");
+    }
+
+
+    public void testNegIntUns() {
+        run(13, -173, 7, false, "    173");
+        run(13, -173, 3, false, "173");
+    }
+
+
+    public void testPosIntSgn() {
+        run(13, 173, 3, true, " 73");
+        run(13, 173, 2, true, " 3");
+    }
+
+
+    public void testZeroIntSgn() {
+        run(13, 0, 7, true, "      0");
+        run(13, 0, 2, true, " 0");
+    }
+
+
+    public void testNegIntSgn() {
+        run(13, -173, 7, true, "   -173");
+        run(13, -173, 3, true, "-73");
+        run(13, -173, 2, true, "-3");
+    }
+
+
+    // ******************************************************************** //
+    // Float Tests.
+    // ******************************************************************** //
+
+    public void testPosFloatUns() {
+        run(13, 173.45678, 7, 2, false, " 173.45");
+        run(13, 173.45678, 7, 3, false, "173.456");
+        run(13, 73.00678, 7, 4, false, "73.0067");
+    }
+
+
+    public void testZeroFloatUns() {
+        run(13, 0, 7, 2, false, "   0.00");
+        run(13, 0, 7, 3, false, "  0.000");
+    }
+
+
+    public void testNegFloatUns() {
+        run(13, -173.45678, 7, 2, false, " 173.45");
+        run(13, -173.45678, 7, 3, false, "173.456");
+        run(13, -73.00678, 7, 4, false, "73.0067");
+    }
+
+
+    public void testPosFloatSgn() {
+        run(13, 173.45678, 7, 2, true, " 173.45");
+        run(13, 173.45678, 7, 3, true, " 73.456");
+        run(13, 73.00678, 7, 4, true, " 3.0067");
+    }
+
+
+    public void testZeroFloatSgn() {
+        run(13, 0, 7, 2, true, "   0.00");
+        run(13, 0, 7, 3, true, "  0.000");
+    }
+
+
+    public void testNegFloatSgn() {
+        run(13, -173.45678, 7, 2, true, "-173.45");
+        run(13, -173.45678, 7, 3, true, "-73.456");
+        run(13, -73.00678, 7, 4, true, "-3.0067");
+    }
+
+
+    public void testSpeed() {
+        final int COUNT = 100000;
+        
+        long j1 = System.currentTimeMillis();
+        for (int i = 0; i < COUNT; ++i)
+            String.format("%7.2f", -(i / 1345678f));
+        long j2 = System.currentTimeMillis();
+        
+        long c1 = System.currentTimeMillis();
+        for (int i = 0; i < COUNT; ++i)
+            CharFormatter.formatFloat(buf, 13, -(i / 1345678f), 7, 2, true);
+        long c2 = System.currentTimeMillis();
+        
+        // CharFormatter should be at least 10 times faster.
+        assertTrue((c2 - c1) * 10 < (j2 - j1));
+    }
+
+
+    // ******************************************************************** //
+    // Member Data.
+    // ******************************************************************** //
+
+    private char[] buf;
+
 }
 
