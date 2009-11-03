6502a37a553c53758d080104ee17946c53e526f6
headstay <headstay@3248975e-79f9-11de-8a13-ad0aff062799>
2009-11-03 01:41:49 +0000

    Added DSP.

---
+++ Hermit Library/src/org/hermit/dsp/FFTTransformer.java
@@ -0,0 +1,236 @@
+
+/**
+ * dsp: various digital signal processing algorithms
+ * <br>Copyright 2009 Ian Cameron Smith
+ *
+ * <p>This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2
+ * as published by the Free Software Foundation (see COPYING).
+ * 
+ * <p>This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+
+package org.hermit.dsp;
+
+
+/**
+ * Implementation of the Cooley–Tukey FFT algorithm by Tsan-Kuang Lee:
+ * http://www.ling.upenn.edu/~tklee/Projects/dsp/
+ * 
+ * His copyright statement: "Do whatever you want with the code.
+ * Feedbacks and improvement welcome."
+ */
+public final class FFTTransformer {
+
+    // ******************************************************************** //
+    // Constructor.
+    // ******************************************************************** //
+
+    /**
+     * Create an FFT transformer for a given sample size.  This preallocates
+     * resources appropriate to that block size.
+     * 
+     * @param   size        The number of samples in a block that we will
+     *                      be asked to transform.  Must be a power of 2.
+     * @throws  IllegalArgumentException    Invalid parameter.
+     */
+    public FFTTransformer(int size) {
+        if (!isPowerOf2(size))
+            throw new IllegalArgumentException("size for FFT must" +
+                                               " be a power of 2 (was " + size + ")");
+        
+        n = size;
+        nu = (int) (Math.log(size) / Math.log(2));
+        xre = new float[n];
+        xim = new float[n];
+    }
+    
+
+    // ******************************************************************** //
+    // Data Setup.
+    // ******************************************************************** //
+
+    /**
+     * Set up a new data block for the FFT algorithm.  The data in
+     * the provided buffer will be copied out, and that buffer
+     * will not be referenced again.  This is separated
+     * out from the main computation to allow for more efficient use
+     * of locks.
+     * 
+     * @param   input       The input data buffer.
+     * @param   off         Offset in the buffer at which the data to
+     *                      be transformed starts.
+     * @param   count       Number of samples in the data to be
+     *                      transformed.  Must be the same as the size
+     *                      parameter that was given to the constructor.
+     * @throws  IllegalArgumentException    Invalid data size.
+     */
+    public final void setInput(float[] input, int off, int count) {
+        if (count != n)
+            throw new IllegalArgumentException("bad input count in FFT:" +
+                                               " constructed for " + n +
+                                               "; given " + input.length);
+       
+        for (int i = 0; i < n; i++) {
+            xre[i] = input[i];
+            xim[i] = 0.0f;
+        }
+    }
+    
+
+    /**
+     * Set up a new data block for the FFT algorithm.  The data in
+     * the provided buffer will be copied out, and that buffer
+     * will not be referenced again.  This is separated
+     * out from the main computation to allow for more efficient use
+     * of locks.
+     * 
+     * @param   input       The input data buffer.
+     * @param   off         Offset in the buffer at which the data to
+     *                      be transformed starts.
+     * @param   count       Number of samples in the data to be
+     *                      transformed.  Must be the same as the size
+     *                      parameter that was given to the constructor.
+     * @throws  IllegalArgumentException    Invalid data size.
+     */
+    public final void setInput(short[] input, int off, int count) {
+        if (count != n)
+            throw new IllegalArgumentException("bad input count in FFT:" +
+                                               " constructed for " + n +
+                                               "; given " + input.length);
+       
+        for (int i = 0; i < n; i++) {
+            xre[i] = (float) input[i] / 32768f;
+            xim[i] = 0.0f;
+        }
+    }
+    
+
+    // ******************************************************************** //
+    // Transform.
+    // ******************************************************************** //
+
+    /**
+     * Transform the data provided in the last call to setInput.
+     */
+    public final void transform() {
+        int n2 = n / 2;
+        int nu1 = nu - 1;
+        float tr, ti, p, arg, c, s;
+        int k = 0;
+
+        for (int l = 1; l <= nu; l++) {
+            while (k < n) {
+                for (int i = 1; i <= n2; i++) {
+                    p = bitrev (k >> nu1, nu);
+                    arg = 2 * (float) Math.PI * p / n;
+                    c = (float) Math.cos (arg);
+                    s = (float) Math.sin (arg);
+                    tr = xre[k+n2]*c + xim[k+n2]*s;
+                    ti = xim[k+n2]*c - xre[k+n2]*s;
+                    xre[k+n2] = xre[k] - tr;
+                    xim[k+n2] = xim[k] - ti;
+                    xre[k] += tr;
+                    xim[k] += ti;
+                    k++;
+                }
+                k += n2;
+            }
+            k = 0;
+            nu1--;
+            n2 = n2/2;
+        }
+        k = 0;
+        int r;
+        while (k < n) {
+            r = bitrev (k, nu);
+            if (r > k) {
+                tr = xre[k];
+                ti = xim[k];
+                xre[k] = xre[r];
+                xim[k] = xim[r];
+                xre[r] = tr;
+                xim[r] = ti;
+            }
+            k++;
+        }
+    }
+
+
+    // ******************************************************************** //
+    // Results.
+    // ******************************************************************** //
+
+    /**
+     * Get the real results of the last transformation.
+     * 
+     * @param   buffer  Buffer in which the real part of the results
+     *                  will be placed.  This buffer must be half the
+     *                  length of the input block.  If transform() has
+     *                  not been called, the results will be garbage.
+     * @return          The parameter buffer.
+     * @throws  IllegalArgumentException    Invalid buffer size.
+     */
+    public final float[] getResults(float[] buffer) {
+        if (buffer.length != n / 2)
+            throw new IllegalArgumentException("bad output buffer size in FFT:" +
+                                               " must be " + (n / 2) +
+                                               "; given " + buffer.length);
+       
+        buffer[0] = (float) (Math.sqrt(xre[0]*xre[0] + xim[0]*xim[0]))/n;
+        for (int i = 1; i < n/2; i++)
+            buffer[i]= 2 * (float) (Math.sqrt(xre[i]*xre[i] + xim[i]*xim[i]))/n;
+        return buffer;
+    }
+
+
+    // ******************************************************************** //
+    // Internal Utilities.
+    // ******************************************************************** //
+
+    /**
+     * Reverse the lowest n bits of j.
+     * 
+     * @param   j       Number to be reversed.
+     * @param   n       Number of low-order bits of j which are significant
+     *                  and to be reversed.
+     */
+    private static final int bitrev(int j, int n) {
+        int j1 = j;
+        int j2;
+        int k = 0;
+        for (int i = 1; i <= n; i++) {
+            j2 = j1 / 2;
+            k  = 2*k + j1 - 2*j2;
+            j1 = j2;
+        }
+        return k;
+    }
+
+    
+    /**
+     * Returns true if the argument is power of 2.
+     * 
+     * @param   n       The number to test.
+     * @return          true if the argument is power of 2.
+     */
+    private static final boolean isPowerOf2(int n) {
+        return (n > 0) && ((n & (n - 1)) == 0);
+    }
+
+    
+    // ******************************************************************** //
+    // Private Data.
+    // ******************************************************************** //
+
+    private final int n;
+    private final int nu;
+    private final float[] xre;
+    private final float[] xim;
+
+}
+
---
+++ Hermit Library/src/org/hermit/dsp/PowerMeter.java
@@ -0,0 +1,129 @@
+
+/**
+ * dsp: various digital signal processing algorithms
+ * <br>Copyright 2009 Ian Cameron Smith
+ *
+ * <p>This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2
+ * as published by the Free Software Foundation (see COPYING).
+ * 
+ * <p>This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+
+package org.hermit.dsp;
+
+
+/**
+ * A power metering algorithm.
+ */
+public final class PowerMeter {
+
+    // ******************************************************************** //
+    // Constructor.
+    // ******************************************************************** //
+
+    /**
+     * Only static methods are provided in this class.
+     */
+    private PowerMeter() {
+    }
+    
+
+    // ******************************************************************** //
+    // Algorithm.
+    // ******************************************************************** //
+
+    /**
+     * Calculate the power of the given input signal.
+     * 
+     * @param   sdata       Input samples to process.
+     * @param   off         Offset in sdata of the data of interest.
+     * @param   samples     Number of data samples to process.
+     * @return              Calculates power in dB; zero represents
+     *                      100dB and 1 is 0dB (maximum power).
+     */
+    public final static float calculatePowerDb(short[] sdata, int off, int samples) {
+        /**
+        Calculate power of the signal depending on format.
+
+        Since the signal may not have an average value of 0 precisely,
+        we shouldn't simply calculate:
+
+        sum_for_all_samples (pulse_value²) / number_of_samples
+
+        but this formula assumes that the average is zero, which is not
+        always true (for example, in 8 bits on a Sound Blaster 64,
+        there is always a shift by one unit.
+
+        We could calculate in two passes, first the average, then the
+        power of the measure minus the average. But we can do this in
+        one pass.
+
+        Let measure = signal + bias,
+        where measure is the pulse value,
+        signal is what we want,
+        bias is a constant, such that the average of signal is zero.
+        
+        What we want is the value of: power = sum_for_all_samples (signal²)
+
+        Let's calculate in the same pass:
+        a=sum_for_all_samples (measure²)
+        and
+        b=sum_for_all_samples (measure)
+        
+        Then a and b are equivalent to:
+        a = sum_for_all_samples (measure²)
+          = sum_for_all_samples ((signal + bias)²)
+          = sum_for_all_samples (signal² + bias²)
+          = sum_for_all_samples (signal²) + number_of_samples * bias²
+      
+        and 
+        b = sum_for_all_samples (measure)
+          = bias * number_of_samples
+        that is, number_of_samples * bias² = b² / number_of_samples
+
+        So a = power + b² / number_of_samples
+
+        And power = a - b² / number_of_samples
+
+        So we've got the correct power of the signal in one pass.
+        
+      */
+
+
+
+        long b = 0;
+        long a = 0;
+        float floatPower = 0;
+        for (int i = 0; i < samples; i++) {
+            /* Since we calculate the square of something that can be
+              as big as +-32767 we assume a width of at least 32 bits
+              for a signed int. Moreover, we add a thousand of these
+              to calculate power, so 32 bits aren't enough. I chose 64
+              bits unsigned int for precision. We could have switched
+              to float or double instead... */
+            final int v = sdata[off + i];
+            /* Note: we calculate max value anyway, to detect clipping */
+            a += (v * v);
+            b += v;
+        }
+
+        /* Ok for raw power. Now normalize it. */
+        long power = a - b * b / samples;
+
+        int maxAmp = 32768;
+        floatPower = ((float) power) / ((float) maxAmp) / ((float) maxAmp) / ((float) samples);
+
+        /* we want leftmost to be 100dB
+          (though signal-to-noise ratio can't be more than 96.33dB in power)
+          and rightmost to be 0dB (maximum power) */
+        float dBvalue = 1f + 0.1f * (float) Math.log10(floatPower);
+        return dBvalue;
+    }
+    
+}
+
