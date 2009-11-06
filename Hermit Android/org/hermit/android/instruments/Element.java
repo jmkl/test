8ffd42643bd65c9760a84201e35f15be715c20a8
headstay <headstay@3248975e-79f9-11de-8a13-ad0aff062799>
2009-11-06 16:31:15 +0000

    Added instruments.

---
+++ Hermit Android/org/hermit/android/instruments/Element.java
@@ -0,0 +1,473 @@
+
+/**
+ * Tricorder: turn your phone into a tricorder.
+ * 
+ * This is an Android implementation of a Star Trek tricorder, based on
+ * the phone's own sensors.  It's also a demo project for sensor access.
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License version 2
+ *   as published by the Free Software Foundation (see COPYING).
+ * 
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ */
+
+
+package org.hermit.android.instruments;
+
+
+import android.app.Activity;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Rect;
+
+
+/**
+ * This class displays an element of the UI.  An element is a region
+ * within a view, and can display text, etc.
+ */
+public class Element
+{
+
+	// ******************************************************************** //
+	// Constructor.
+	// ******************************************************************** //
+	
+	/**
+	 * Set up this view.
+	 * 
+	 * @param	context			Parent application context.
+	 */
+	public Element(Activity context) {
+		appContext = context;
+
+		// Set up our paint.
+		drawPaint = new Paint();
+
+		initializePaint(drawPaint);
+	}
+
+	
+	/**
+	 * Set up this view.
+	 * 
+	 * @param	context			Parent application context.
+     * @param	grid			Colour for drawing a data scale / grid.
+     * @param	plot			Colour for drawing data plots.
+	 */
+	public Element(Activity context, int grid, int plot) {
+		appContext = context;
+		gridColour = grid;
+		plotColour = plot;
+
+		// Set up our paint.
+		drawPaint = new Paint();
+
+		initializePaint(drawPaint);
+	}
+
+
+	/**
+	 * Set up the paint for this element.  This is called during
+	 * initialization.  Subclasses can override this to do class-specific
+	 * one-time initialization.
+	 * 
+	 * @param paint			The paint to initialize.
+	 */
+	protected void initializePaint(Paint paint) { }
+	
+
+    // ******************************************************************** //
+    // Global Layout Parameters.
+    // ******************************************************************** //
+
+    /**
+     * Get the base size for text based on this screen's size.
+     * 
+     * @param   size        Base text size for the app.
+     */
+	public static void setBaseTextSize(float size) {
+        baseTextSize = size;
+    }
+
+
+    /**
+     * Get the base size for text based on this screen's size.
+     * 
+     * @return              Base text size for the app.
+     */
+	public static float getBaseTextSize() {
+        return baseTextSize;
+    }
+
+
+    /**
+     * Get the size for mini text based on this screen's size.
+     * 
+     * @return              Mini text size for the app.
+     */
+	public static float getMiniTextSize() {
+        return baseTextSize - 6;
+    }
+
+
+    /**
+     * Get the size for tiny text based on this screen's size.
+     * 
+     * @return              Tiny text size for the app.
+     */
+	public static float getTinyTextSize() {
+        return baseTextSize - 7;
+    }
+
+
+    /**
+     * Set the sidebar width.
+     * 
+     * @param   width       The sidebar width.
+     */
+	public static void setSidebarWidth(int width) {
+        viewSidebar = width;
+    }
+
+    
+    /**
+     * Get the sidebar width.
+     * 
+     * @return              The sidebar width.
+     */
+	public static int getSidebarWidth() {
+        return viewSidebar;
+    }
+
+
+    /**
+     * Set the amount of padding between major elements in a view.
+     * 
+     * @param   pad     The amount of padding between major elements in a view.
+     */
+	public static void setInterPadding(int pad) {
+        interPadding = pad;
+    }
+
+
+    /**
+     * Get the amount of padding between major elements in a view.
+     * 
+     * @return          The amount of padding between major elements in a view.
+     */
+	public static int getInterPadding() {
+        return interPadding;
+    }
+
+
+    /**
+     * Set the amount of padding within atoms within an element.  Specifically
+     * the small gaps in side bars.
+     * 
+     * @param   gap     The amount of padding within atoms within an element
+     */
+	public static void setInnerGap(int gap) {
+        innerGap = gap;
+    }
+
+
+    /**
+     * Get the amount of padding within atoms within an element.  Specifically
+     * the small gaps in side bars.
+     * 
+     * @return          The amount of padding within atoms within an element
+     */
+	public static int getInnerGap() {
+        return innerGap;
+    }
+
+
+    // ******************************************************************** //
+	// Geometry.
+	// ******************************************************************** //
+
+    /**
+     * This is called during layout when the size of this element has
+     * changed.  This is where we first discover our size, so set
+     * our geometry to match.
+     * 
+	 * @param	bounds		The bounding rect of this element within
+	 * 						its parent View.
+     */
+	public void setGeometry(Rect bounds) {
+		elemBounds = bounds;
+	}
+
+	
+	/**
+	 * Get the minimum preferred width for this atom.
+	 * 
+	 * @return			The minimum preferred width for this atom.
+	 * 					Returns zero if we don't know yet.
+	 */
+	public int getPreferredWidth() {
+		return 0;
+	}
+	
+
+	/**
+	 * Get the minimum preferred height for this atom.
+	 * 
+	 * @return			The minimum preferred height for this atom.
+	 * 					Returns zero if we don't know yet.
+	 */
+	public int getPreferredHeight() {
+		return 0;
+	}
+	
+
+	/**
+	 * Get the bounding rect of this Element.
+	 * 
+	 * @return				The bounding rect of this element within
+	 * 						its parent View.  This will be 0, 0, 0, 0 if
+	 * 						setGeometry() has not been called yet.
+	 */
+	public final Rect getBounds() {
+		return elemBounds;
+	}
+
+	
+	/**
+	 * Get the width of this element -- i.e. the current configured width.
+	 * 
+	 * @return				The width of this element within
+	 * 						its parent View.  This will be 0 if
+	 * 						setGeometry() has not been called yet.
+	 */
+	public final int getWidth() {
+		return elemBounds.right - elemBounds.left;
+	}
+	
+	
+	/**
+	 * Get the height of this element -- i.e. the current configured height.
+	 * 
+	 * @return				The height of this element within
+	 * 						its parent View.  This will be 0 if
+	 * 						setGeometry() has not been called yet.
+	 */
+	public final int getHeight() {
+		return elemBounds.bottom - elemBounds.top;
+	}
+	
+
+    // ******************************************************************** //
+    // Appearance.
+    // ******************************************************************** //
+
+	/**
+	 * Set the background colour of this element.
+	 * 
+	 * @param	col			The new background colour, in ARGB format.
+	 */
+	public void setBackgroundColor(int col) {
+		colBg = col;
+	}
+	
+
+	/**
+	 * Get the background colour of this element.
+	 * 
+	 * @return				The background colour, in ARGB format.
+	 */
+	public int getBackgroundColor() {
+		return colBg;
+	}
+	
+
+	/**
+	 * Set the plot colours of this element.
+	 * 
+     * @param	grid      Colour for drawing a data scale / grid.
+     * @param	plot      Colour for drawing data plots.
+	 */
+	public void setDataColors(int grid, int plot) {
+		gridColour = grid;
+		plotColour = plot;
+	}
+	
+
+    /**
+     * Set the data scale / grid colour of this element.
+     * 
+     * @param   grid      Colour for drawing a data scale / grid.
+     */
+    public void setGridColor(int grid) {
+        gridColour = grid;
+    }
+    
+
+    /**
+     * Set the data plot colour of this element.
+     * 
+     * @param   plot      Colour for drawing a data plot.
+     */
+    public void setPlotColor(int plot) {
+        plotColour = plot;
+    }
+    
+
+    /**
+     * Get the data scale / grid colour of this element.
+     * 
+     * @return            Colour for drawing a data scale / grid.
+     */
+    public int getGridColor() {
+        return gridColour;
+    }
+    
+
+    /**
+     * Get the data plot colour of this element.
+     * 
+     * @return          Colour for drawing data plots.
+     */
+    public int getPlotColor() {
+        return plotColour;
+    }
+    
+
+	// ******************************************************************** //
+	// View Drawing.
+	// ******************************************************************** //
+	
+	/**
+	 * Get this element's Paint.
+	 * 
+	 * @return				The Paint which was set up in initializePaint().
+	 */
+	protected Paint getPaint() {
+		return drawPaint;
+	}
+	
+	
+	/**
+	 * This method is called to ask the element to draw itself.
+	 * 
+	 * @param	canvas		Canvas to draw into.
+	 * @param	now				Current system time in ms.
+	 */
+	public void draw(Canvas canvas, long now) {
+		drawStart(canvas, drawPaint);
+		drawBody(canvas, drawPaint);
+		drawFinish(canvas, drawPaint);
+	}
+
+
+	/**
+	 * Do initial parts of drawing for this element.
+	 * 
+	 * @param	canvas		Canvas to draw into.
+	 * @param	paint		The Paint which was set up in initializePaint().
+	 */
+	protected void drawStart(Canvas canvas, Paint paint) {
+		// Clip to our part of the canvas.
+		canvas.save();
+		canvas.clipRect(getBounds());
+	}
+
+	
+	/**
+	 * Do the subclass-specific parts of drawing for this element.
+	 * 
+	 * Subclasses should override this to do their drawing.
+	 * 
+	 * @param	canvas		Canvas to draw into.
+	 * @param	paint		The Paint which was set up in initializePaint().
+	 */
+	protected void drawBody(Canvas canvas, Paint paint) {
+		// If not overridden, just fill with BG colour.
+		canvas.drawColor(colBg);
+	}
+	
+
+	/**
+	 * Wrap up drawing of this element.
+	 * 
+	 * @param	canvas		Canvas to draw into.
+	 * @param	paint		The Paint which was set up in initializePaint().
+	 */
+	protected void drawFinish(Canvas canvas, Paint paint) {
+		canvas.restore();
+	}
+
+	
+	// ******************************************************************** //
+	// Utilities.
+	// ******************************************************************** //
+
+	/**
+	 * Get the app context of this Element.
+	 * 
+	 * @return             The app context we're running in.
+	 */
+	protected Activity getContext() {
+		return appContext;
+	}
+	
+
+	/**
+	 * Get the String value of a resource.
+	 * 
+	 * @param  resid       The ID of the resource we want.
+	 * @return             The resource value.
+	 */
+	protected String getRes(int resid) {
+		return appContext.getString(resid);
+	}
+	
+
+	// ******************************************************************** //
+	// Class Data.
+	// ******************************************************************** //
+
+    // Debugging tag.
+	@SuppressWarnings("unused")
+	private static final String TAG = "tricorder";
+
+    // The base size for all text, based on screen size.
+    private static float baseTextSize = 0f;
+
+    // The thickness of a side bar in a view element.
+    private static int viewSidebar;
+    
+    // The amount of padding between major elements in a view.
+    private static int interPadding;
+    
+    // The amount of padding within atoms within an element.  Specifically
+    // the small gaps in side bars.
+    private static int innerGap;
+
+
+	// ******************************************************************** //
+	// Private Data.
+	// ******************************************************************** //
+
+	// Application handle.
+	private final Activity appContext;
+	
+	// The paint we use for drawing.
+	private Paint drawPaint = null;
+
+	// The bounding rect of this element within its parent View.
+	private Rect elemBounds = new Rect(0, 0, 0, 0);
+
+	// Background colour.
+	private int colBg = 0xff000000;
+
+	// Colour of the graph grid and plot.
+	private int gridColour = 0xff00ff00;
+	private int plotColour = 0xffff0000;
+
+}
+
---
+++ Hermit Android/org/hermit/android/instruments/TextAtom.java
@@ -0,0 +1,403 @@
+
+/**
+ * Tricorder: turn your phone into a tricorder.
+ * 
+ * This is an Android implementation of a Star Trek tricorder, based on
+ * the phone's own sensors.  It's also a demo project for sensor access.
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License version 2
+ *   as published by the Free Software Foundation (see COPYING).
+ * 
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ */
+
+
+package org.hermit.android.instruments;
+
+
+import android.app.Activity;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.Typeface;
+
+
+/**
+ * This class displays an element of the UI.  An element is a region
+ * within a view, and can display text, etc.
+ */
+public class TextAtom
+	extends Element
+{
+
+	// ******************************************************************** //
+	// Constructor.
+	// ******************************************************************** //
+	
+    /**
+     * Set up this view, and configure the text fields to be displayed in
+     * this element.  This is equivalent to calling setTextFields()
+     * after the basic constructor.
+     * 
+     * We support display of a single field, or a rectangular table
+     * of fields.  The caller must call
+     * {@link #setTextFields(String[] template, int rows)} to set the
+     * table format.
+     * 
+     * @param   context     Parent application context.
+     */
+    public TextAtom(Activity context) {
+        super(context);
+        textSize = getBaseTextSize();
+    }
+
+
+    /**
+     * Set up this view, and configure the text fields to be displayed in
+     * this element.  This is equivalent to calling setTextFields()
+     * after the basic constructor.
+     * 
+     * We support display of a single field, or a rectangular table
+     * of fields.  The fields are specified by passing in sample text
+     * values to be measured; we then allocate the space automatically.
+     * 
+     * @param   context     Parent application context.
+     * @param   template    Strings representing the columns to display.
+     *                      Each one should be a sample piece of text
+     *                      which will be measured to determine the
+     *                      required space for each column.  Must be provided.
+     * @param   rows        Number of rows of text to display.
+     */
+    public TextAtom(Activity context, String[] template, int rows) {
+        super(context);
+        textSize = getBaseTextSize();
+        
+        // Set up the text fields.
+        setTextFields(template, rows);
+    }
+
+
+	/**
+	 * Set up the paint for this element.  This is called during
+	 * initialization.  Subclasses can override this to do class-specific
+	 * one-time initialization.
+	 * 
+	 * @param paint			The paint to initialize.
+	 */
+	@Override
+	protected void initializePaint(Paint paint) {
+		paint.setTextScaleX(FONT_SCALEX);
+		paint.setTypeface(FONT_FACE);
+		paint.setAntiAlias(true);
+	}
+	
+	   
+    // ******************************************************************** //
+	// Geometry.
+	// ******************************************************************** //
+
+    /**
+     * This is called during layout when the size of this element has
+     * changed.  This is where we first discover our size, so set
+     * our geometry to match.
+     * 
+	 * @param	bounds		The bounding rect of this element within
+	 * 						its parent View.
+     */
+	@Override
+	public void setGeometry(Rect bounds) {
+		super.setGeometry(bounds);
+
+		// Position our text based on our actual geometry.  If setTextFields()
+		// hasn't been called this does nothing.
+		positionText();
+	}
+
+
+    /**
+     * Set the margins around the displayed text.  This the total space
+     * between the edges of the element and the outside bounds of the text.
+     * 
+	 * @param	left		The left margin.
+	 * @param	top			The top margin.
+	 * @param	right		The right margin.
+	 * @param	bottom		The bottom margin.
+     */
+	public void setMargins(int left, int top, int right, int bottom) {
+		marginLeft = left;
+		marginTop = top;
+		marginRight = right;
+		marginBottom = bottom;
+		
+		// Position our text based on these new margins.  If setTextFields()
+		// hasn't been called this does nothing.
+		positionText();
+	}
+
+
+	/**
+	 * Set up the text fields to be displayed in this element.
+	 * If this is never called, there will be no text.
+	 * 
+	 * We support display of a single field, or a rectangular table
+	 * of fields.  The fields are specified by passing in sample text
+	 * values to be measured; we then allocate the space automatically.
+	 * 
+	 * This must be called before setText() can be called.
+	 * 
+	 * @param	template	Strings representing the columns to display.
+	 * 						Each one should be a sample piece of text
+	 * 						which will be measured to determine the
+	 * 						required space for each column.
+     * @param   rows        Number of rows of text to display.
+	 */
+	public void setTextFields(String[] template, int rows) {
+		fieldTemplate = template;
+        numRows = rows;
+
+        // Make the field buffers based on the template.
+        char[][][] buffers = new char[numRows][][];
+        for (int r = 0; r < numRows; ++r) {
+            int cols = template.length;
+            buffers[r] = new char[cols][];
+            for (int c = 0; c < cols; ++c)
+                buffers[r][c] = new char[template[c].length()];
+        }
+        fieldBuffers = buffers;
+
+		// Position our text based on the template.  If setGeometry()
+		// hasn't been called yet, then the positions will not be final,
+		// but getTextWidth() and getTextHeight() will return sensible
+		// values.
+		positionText();
+	}
+	
+	
+	/**
+	 * Get the text buffers for the field values.  The caller can change
+	 * a field's content by writing to the appropriate member of the
+	 * array, as in "buffer[row][col][0] = 'X';".
+	 * 
+	 * @return             Text buffers for the field values.
+	 */
+	public char[][][] getBuffer() {
+	    return fieldBuffers;
+	}
+	
+	
+	/**
+	 * Get the minimum width needed to fit all the text.
+	 * 
+	 * @return			The minimum width needed to fit all the text.
+	 * 					Returns zero if setTextFields() hasn't been called.
+	 */
+	@Override
+	public int getPreferredWidth() {
+		return textWidth;
+	}
+	
+
+	/**
+	 * Get the minimum height needed to fit all the text.
+	 * 
+	 * @return			The minimum height needed to fit all the text.
+	 * 					Returns zero if setTextFields() hasn't been called.
+	 */
+	@Override
+	public int getPreferredHeight() {
+		return textHeight;
+	}
+
+
+	/**
+	 * Position the text based on the current template and geometry.
+	 * If If setTextFields() hasn't been called this does nothing.
+	 * If setGeometry() hasn't been called yet, then the positions will
+	 * not be final, but getTextWidth() and getTextHeight() will return
+	 * sensible values.
+	 */
+	private void positionText() {
+		if (fieldTemplate == null)
+			return;
+		
+		final int nf = fieldTemplate.length;
+		colsX = new int[nf];
+		rowsY = new int[numRows];
+		
+		Rect bounds = getBounds();
+		Paint paint = getPaint();
+		paint.setTextSize(textSize);
+
+		// Assign all the column positions based on minimum width.
+		int x = bounds.left;
+		for (int i = 0; i < nf; ++i) {
+			int len = (int) Math.ceil(paint.measureText(fieldTemplate[i]));
+			int lp = i > 0 ? textPadLeft : marginLeft;
+			int rp = i < nf - 1 ? textPadRight : marginRight;
+			colsX[i] = x + lp;
+			x += len + lp + rp;
+		}
+		textWidth = x - bounds.left;
+		
+		// If we have excess width, distribute it into the inter-column gaps.
+		// Don't adjust textWidth because it is the minimum.
+		if (nf > 1) {
+			int excess = (bounds.right - x) / (nf - 1);
+			if (excess > 0) {
+				for (int i = 1; i < nf; ++i)
+					colsX[i] += excess * i;
+			}
+		}
+		
+		// Assign all the row positions based on minimum height.
+ 	   	Paint.FontMetricsInt fm = paint.getFontMetricsInt();
+		int y = bounds.top;
+		for (int i = 0; i < numRows; ++i) {
+			int tp = i > 0 ? textPadTop : marginTop;
+			int bp = i < numRows - 1 ? textPadBottom : marginBottom;
+			rowsY[i] = y + tp - fm.ascent - 2;
+			y += -fm.ascent - 2 + fm.descent + tp + bp;
+		}
+		textHeight = y - bounds.top;
+	}
+	
+	
+    // ******************************************************************** //
+	// Appearance.
+	// ******************************************************************** //
+
+	/**
+	 * Set the text colour of this element.
+	 * 
+	 * @param	col			The new text colour, in ARGB format.
+	 */
+	public void setTextColor(int col) {
+		setPlotColor(col);
+	}
+	
+
+	/**
+	 * Get the text colour of this element.
+	 * 
+	 * @return				The text colour, in ARGB format.
+	 */
+	public int getTextColor() {
+		return getPlotColor();
+	}
+	
+
+	/**
+	 * Set the text size of this element.
+	 * 
+	 * @param	size		The new text size.
+	 */
+	public void setTextSize(float size) {
+		textSize = size;
+		
+		// Position our text based on the new size.  If setTextFields()
+		// hasn't been called this does nothing.
+		positionText();
+	}
+	
+
+	/**
+	 * Get the text size of this element.
+	 * 
+	 * @return				The text size.
+	 */
+	public float getTextSize() {
+		return textSize;
+	}
+	
+
+	// ******************************************************************** //
+	// View Drawing.
+	// ******************************************************************** //
+	
+	/**
+	 * This method is called to ask the element to draw itself.
+	 * 
+	 * @param	canvas		Canvas to draw into.
+	 * @param	paint		The Paint which was set up in initializePaint().
+	 */
+	@Override
+	protected void drawBody(Canvas canvas, Paint paint) {
+		// Set up the display style.
+		paint.setColor(getPlotColor());
+		paint.setTextSize(textSize);
+
+		final char[][][] tv = fieldBuffers;
+		
+		// If we have any text to show, draw it.
+		if (tv != null) {
+			for (int row = 0; row < rowsY.length && row < tv.length; ++row) {
+			    char[][] fields = tv[row];
+				int y = rowsY[row];
+				for (int col = 0; col < colsX.length && col < fields.length; ++col) {
+				    char[] field = fields[col];
+					int x = colsX[col];
+					canvas.drawText(field, 0, field.length, x, y, paint);
+				}
+			}
+		}
+	}
+
+	
+    // ******************************************************************** //
+    // Class Data.
+    // ******************************************************************** //
+
+    // Debugging tag.
+	@SuppressWarnings("unused")
+	private static final String TAG = "tricorder";
+
+	// Horizontal scaling of the font; used to produce a tall, thin font.
+	private static final Typeface FONT_FACE = Typeface.MONOSPACE;
+
+	// Horizontal scaling of the font; used to produce a tall, thin font.
+	private static final float FONT_SCALEX = 0.6f;
+	
+	
+	// ******************************************************************** //
+	// Private Data.
+	// ******************************************************************** //
+	
+	// Template for the text fields we're displaying.
+	private String[] fieldTemplate = null;
+	private int numRows = 0;
+	
+	// Buffers where the values of the fields will be stored.
+    private char[][][] fieldBuffers;
+
+	// Horizontal positions of the text columns, and vertical positions
+	// of the rows.  These are the actual text base positions.  These
+	// will be null if we have no defined text fields.
+	private int[] colsX = null;
+	private int[] rowsY = null;
+	
+	// The width and height we would need to display all the text at minimum,
+	// including padding and margins.  Set after a call to setTextFields().
+	private int textWidth = 0;
+	private int textHeight = 0;
+	
+	// Current text size.
+	private float textSize;
+
+	// Margins.  These are applied around the outside of the text.
+	private int marginLeft = 0;
+	private int marginRight = 0;
+	private int marginTop = 0;
+	private int marginBottom = 0;
+
+	// Text padding.  This is applied between all pairs of text fields.
+	private int textPadLeft = 2;
+	private int textPadRight = 2;
+	private int textPadTop = 0;
+	private int textPadBottom = 0;
+	
+}
+
