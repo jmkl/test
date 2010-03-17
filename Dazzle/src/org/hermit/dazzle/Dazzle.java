55c7fc5eb92f84fdd0c3ca1825b5b04654a8e3d8
headstay <headstay@3248975e-79f9-11de-8a13-ad0aff062799>
2010-03-17 02:10:25 +0000

    Dazzle: project created.

---
+++ Dazzle/src/org/hermit/dazzle/Dazzle.java
@@ -0,0 +1,13 @@
+package org.hermit.dazzle;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Dazzle extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
