d2cc8d5a379464f6fd637c9516962058815772ea
headstay <headstay@3248975e-79f9-11de-8a13-ad0aff062799>
2010-06-26 23:44:45 +0000

    Audalyzer: update to new library names.

--- Audalyzer/src/org/hermit/audalyzer/Audalyzer.java
+++ Audalyzer/src/org/hermit/audalyzer/Audalyzer.java
@@ -73,10 +73,9 @@ public class Audalyzer
         createEulaBox(R.string.eula_title, R.string.eula_text, R.string.button_close);       
 
         // Set up the standard dialogs.
-        createMessageBox(R.string.button_close);
         setAboutInfo(R.string.about_text);
-        setHomeInfo(R.string.button_homepage, R.string.url_homepage);
-        setLicenseInfo(R.string.button_license, R.string.url_license);
+        setHomeInfo(R.string.url_homepage);
+        setLicenseInfo(R.string.url_license);
 
         // We don't want a title bar or status bar.
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
