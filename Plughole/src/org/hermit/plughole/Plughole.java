c46fe567cdc05ed2cb3a48a0a1d98a3fb793a300
headstay <headstay@3248975e-79f9-11de-8a13-ad0aff062799>
2010-06-26 16:18:15 +0000

    Plughole: use new HermitAndroid library.

--- Plughole/src/org/hermit/plughole/Plughole.java
+++ Plughole/src/org/hermit/plughole/Plughole.java
@@ -120,10 +120,9 @@ public class Plughole
         powerManager = (PowerManager) getSystemService(POWER_SERVICE);
 
         // Create the message and info boxes.
-        createMessageBox(R.string.button_close);
         setAboutInfo(R.string.about_text);
-        setHomeInfo(R.string.button_homepage, R.string.url_homepage);
-        setLicenseInfo(R.string.button_license, R.string.url_license);
+        setHomeInfo(R.string.url_homepage);
+        setLicenseInfo(R.string.url_license);
 
         // We don't want a title bar or status bar.
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
